735963443c3c28db3ef7bb5ab8f63f21ee64beca
CHAITANYA BHATIA <117209756+chaitaanyaaaa@users.noreply.github.com>
2024-06-04 01:22:12 +0530

    first commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useContext,useState } from 'react'
+import UserContext from '../context/UserContext'
+
+function Profile() {
+    const{user}=useContext(UserContext)
+
+    if(!user)
+        return(
+    <div>Please Login</div>)
+  return (
+    <div>Welcome: {user.username}</div>
+  )
+}
+
+export default Profile
